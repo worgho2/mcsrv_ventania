70a02bbd8dab98576541d27c529a6369fb3b5a24
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2023-11-07 13:32:09 -0300

    sync: 2023-11-07T16:32:09Z

--- packages/server/scripts/sync.sh
+++ packages/server/scripts/sync.sh
@@ -14,7 +14,7 @@ if [ -z "$(git status --porcelain)" ]; then
     exit 0
 fi
 
-# Commit and Push changes from packages/server
-git add ./packages/server
+# Commit and Push changes from packages/server to origin
+git add .
 git commit -m "sync: $(date -u +%Y-%m-%dT%H:%M:%SZ)"
 git push
