bcb60dbfc6febcc2f334dd09e91ecbfa1b7ce039
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2023-11-07 13:48:08 -0300

    sync: 2023-11-07T16:48:08Z

--- packages/server/scripts/sync.sh
+++ packages/server/scripts/sync.sh
@@ -2,19 +2,20 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ROOT_DIR=$SCRIPT_DIR/..
+LOG_FILE=~/sync.log
 
 cd $ROOT_DIR
 
-# Pull changes
-git pull
+git pull | tee -a $LOG_FILE
 
 # Abort if there are no changes to commit
 if [ -z "$(git status --porcelain)" ]; then
-    echo "No changes to commit"
+    echo "No changes to commit" | tee -a $LOG_FILE
     exit 0
 fi
 
-# Commit and Push changes from packages/server to origin
-git add .
-git commit -m "sync: $(date -u +%Y-%m-%dT%H:%M:%SZ)"
-git push
+# Commit and Push changes from packages/server to origin and log the execution to a file
+git add . | tee -a $LOG_FILE
+git commit -m "sync: $(date -u +%Y-%m-%dT%H:%M:%SZ)" | tee -a $LOG_FILE
+git push | tee -a $LOG_FILE
+
